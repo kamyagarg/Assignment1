3fae494a3041c6a8aaab8ab8a5615085cb13b00a
Kamya Garg <30886166+kamyagarg@users.noreply.github.com>
2020-12-28 19:04:10 +0530

    Add files via upload
    - 'Service one' text aligned
    - Text inside general box isnt wrapping unnecessarily

--- src/js/webpage.js
+++ src/js/webpage.js
@@ -9,46 +9,49 @@ let bottomboxes = document.getElementsByClassName("bottom1");
 
 
 //Bell Animation 
- let bell_not = document.getElementsByClassName("notification")[0];
- bell_not.onmouseover = function() {bellAnimation()};
- bell_not.onmouseout = function() {bellAnimation_stop()};
+let bell_not = document.getElementsByClassName("notification")[0];
+bell_not.onmouseover = function () { bellAnimation() };
+bell_not.onmouseout = function () { bellAnimation_stop() };
 
-function bellAnimation(){
-    bell_not.classList.add("bell-animation");   
+function bellAnimation() {
+    bell_not.classList.add("bell-animation");
 }
-function bellAnimation_stop(){
-    bell_not.classList.remove("bell-animation");   
+function bellAnimation_stop() {
+    bell_not.classList.remove("bell-animation");
 }
+
+////////////////
 
 
 
 // Overview, Access Control and all, Tab Selection
 let tab_selected = document.getElementsByClassName("bg-list");
-for (let i = 0; i < tab_selected.length; i++){
+for (let i = 0; i < tab_selected.length; i++) {
     tab_selected[i].addEventListener("click",
-    function () {
-        let x = document.querySelector(".bg-list-pink");
-        if(x){
-            x.classList.remove("bg-list-pink");
-        }
-        tab_selected[i].classList.add("bg-list-pink");
-    });
+        function () {
+            let x = document.querySelector(".bg-list-pink");
+            if (x) {
+                x.classList.remove("bg-list-pink");
+            }
+            tab_selected[i].classList.add("bg-list-pink");
+        });
 }
 
+////////////////
 
 // SideNav Open/close using on button
 function openSideNav() {
     //Option 1: using .toggle
     sidenav.classList.toggle("open-sidenav");
 
-    //Option2: using if else
+    //Option2: using conditional statement
     // let status = document.querySelector(".open-sidenav");
     // if(status){
     //     sidenav.classList.remove("open-sidenav");
     // }else{
     //     sidenav.classList.add("open-sidenav");
     // }
-    
+
 }
 
 // You can also close by clicking on the cross button 
@@ -56,12 +59,17 @@ function closeSideNav() {
     sidenav.classList.remove("open-sidenav");
 }
 
-window.onclick = function (event) {
-    if (event.target != sidenav) {
-        sidenav.style.transform = "translateX(100%)";
-    }
-}
 
+// window.onclick = function (event){
+//     if (event.target !== sidenav && sidenav.classList.contains("open-sidenav")) {
+//         sidenav.classList.remove("open-sidenav");
+//     }
+
+// }
+
+
+
+//////////////////
 
 //Sidenav - selected box
 
@@ -69,48 +77,51 @@ window.onclick = function (event) {
 let api_box = document.getElementsByClassName("apiOr")[0];
 let box_selected = document.getElementsByClassName("small-api");
 
-for (let i = 0; i < box_selected.length; i++){
+for (let i = 0; i < box_selected.length; i++) {
     box_selected[i].addEventListener("click",
-    function () {
-        let x = api_box.querySelector(".active-pink");
-        if(x){
-            x.classList.remove("active-pink");
-            
-        }
-        box_selected[i].classList.add("active-pink");
-    });
+        function () {
+            let x = api_box.querySelector(".active-pink");
+            if (x) {
+                x.classList.remove("active-pink");
+
+            }
+            box_selected[i].classList.add("active-pink");
+        });
 }
 
 //Language Row
 let lang_box = document.getElementsByClassName("language")[0];
 let box_selected2 = document.getElementsByClassName("small-lang");
 
-for (let i = 0; i < box_selected2.length; i++){
+for (let i = 0; i < box_selected2.length; i++) {
     box_selected2[i].addEventListener("click",
-    function () {
-        let x = lang_box.querySelector(".active-pink");
-        if(x){
-            x.classList.remove("active-pink");
-        }
-        box_selected2[i].classList.add("active-pink");
-    });
+        function () {
+            let x = lang_box.querySelector(".active-pink");
+            if (x) {
+                x.classList.remove("active-pink");
+            }
+            box_selected2[i].classList.add("active-pink");
+        });
 }
 
 //Git Row
 let git_box = document.getElementsByClassName("Gitlab-left")[0];
 let box_selected3 = document.getElementsByClassName("small-git");
 
-for (let i = 0; i < box_selected3.length; i++){
+for (let i = 0; i < box_selected3.length; i++) {
     box_selected3[i].addEventListener("click",
-    function () {
-        let x = git_box.querySelector(".active-pink");
-        if(x){
-            x.classList.remove("active-pink");
-        }
-        box_selected3[i].classList.add("active-pink");
-    });
+        function () {
+            let x = git_box.querySelector(".active-pink");
+            if (x) {
+                x.classList.remove("active-pink");
+            }
+            box_selected3[i].classList.add("active-pink");
+        });
 }
 
+
+
+///////////////////
 
 // Configure button Functionality
 
@@ -129,20 +140,27 @@ window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+    if ((event.target == sidenav) && (sidenav.classList.contains("open-sidenav"))) {
+        sidenav.classList.remove("open-sidenav");
+    }
 }
 
 
+///////////////////
+
 // Large Thumbnail for bottom boxes / Hiding the table view
-function largeThumbnail(){
+function largeThumbnail() {
     document.getElementsByClassName("table")[0].classList.add("table-hide");
     document.getElementsByClassName("rectangle-26")[0].classList.remove("table-hide");
     document.getElementsByClassName("bottom1")[0].classList.remove("table-hide");
 }
 
 //List/Table view for bottom boxes
-function listView(){
+function listView() {
     document.getElementsByClassName("table")[0].classList.remove("table-hide");
     document.getElementsByClassName("rectangle-26")[0].classList.add("table-hide");
     document.getElementsByClassName("bottom1")[0].classList.add("table-hide");
 }
 
+
+/////////////////////
